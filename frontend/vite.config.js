2cabf829002152a7a0fc3ae5b67e93eb3e65db81
mkhnoori <mkh_noori@yahoo.com>
2025-02-10 21:24:53 -0500

    configfile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: './frontend',  // Set the root to your 'frontend' folder
+  root: './', // set the root to the project's root
   build: {
-    outDir: '../dist',  // Set the output directory for the build
+    outDir: 'dist', // you can specify the output directory here
+    rollupOptions: {
+      input: './index.html', // explicitly set the path to your index.html
+    },
   },
-});
+})
